51490d8d86ce8b7275a56a44334f234b87800895
Robert <49005401+Gitznik@users.noreply.github.com>
2025-12-15 16:43:52 +0100

    Apply suggestions from code review
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- 02/main.go
+++ 02/main.go
@@ -8,9 +8,9 @@ import (
 
 func main() {
 	inpd1 := util.ReadInput(util.POne)
-	fmt.Println("Running day 1")
+	fmt.Println("Running part 1")
 	p1(inpd1)
 
-	fmt.Println("Running day 2")
+	fmt.Println("Running part 2")
 	p2(inpd1)
 }
